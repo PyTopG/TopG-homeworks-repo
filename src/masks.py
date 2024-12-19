556d25ab4911444668866accd23db00f1c04fc4a
Danila Komolov <inostranec1.1@yandex.ru>
2024-12-19 18:40:09 +0400

    homework-10-2 DONE

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,20 @@
-from typing import Union
-
-
-def get_mask_card_number(card_num: Union[int]) -> Union[str]:
+def get_mask_card_number(card_num: int | str) -> str:
     """Функция, которая возвращает маску номера карты"""
+    if not card_num:
+        raise ValueError("Вы ничего не ввели")
+    if not isinstance(card_num, (int | str)):
+        raise TypeError("Неверный формат ввода данных")
+    if len(str(card_num)) != 16:
+        raise ValueError("Неверное количество введенных символов")
     return f"{str(card_num)[:4]} {str(card_num)[4:6]}** **** {str(card_num)[-4:]}"
 
 
-def get_mask_account(account_num: Union[int]) -> Union[str]:
+def get_mask_account(account_num: int | str) -> str:
     """Функция, которая возвращает маску номера счета"""
+    if not account_num:
+        raise ValueError("Вы ничего не ввели")
+    if not isinstance(account_num, (int | str)):
+        raise TypeError("Неверный формат ввода данных")
+    if len(str(account_num)) != 20:
+        raise ValueError("Неверное количество введенных символов")
     return f"**{str(account_num)[-4:]}"
